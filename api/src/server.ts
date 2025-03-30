55ffd975da1b048ea519e84e71b104f15fa1906e
jp kam <jpkamdem@proton.me>
2025-03-30 20:45:59 +0200

    commit initial

---
+++ api/src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.json({ message: "pong" });
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé : http://127.0.0.1:${port}`);
+});
